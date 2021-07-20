6dc19bd11dc128f528fbc1fa8faba649eb9b7200
Yasser Abdelmonem <yasseramonem@gmail.com>
2021-07-20 12:02:17 +0200

    First Build
    Added the Coordinator Layout and a Fab
    
    implemented an onclick on the Fab and added an observer in HomeFragment.kt

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -23,6 +26,17 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.home_fragment, container, false)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        return view
+        val binding = HomeFragmentBinding.inflate(inflater)
+
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer {
+            if (it == true){
+                findNavController().navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        })
+
+        return binding.root
     }
 }
