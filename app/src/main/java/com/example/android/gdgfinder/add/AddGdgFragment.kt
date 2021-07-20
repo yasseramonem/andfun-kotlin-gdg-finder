bef48e568138aa57f1e623aaa963f1a319665493
Yasser Abdelmonem <yasseramonem@gmail.com>
2021-07-20 14:16:48 +0200

    Added Chip & Chip Group

--- app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
@@ -1,13 +1,12 @@
 package com.example.android.gdgfinder.add
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.AddGdgFragmentBinding
 import com.google.android.material.snackbar.Snackbar
@@ -27,16 +26,20 @@ class AddGdgFragment : Fragment() {
 
         binding.viewModel = viewModel
 
-        viewModel.showSnackBarEvent.observe(this, Observer {
+        viewModel.showSnackBarEvent.observe(viewLifecycleOwner, Observer {
             if (it == true) { // Observed state is true.
                 Snackbar.make(
-                    activity!!.findViewById(android.R.id.content),
+                    requireActivity().findViewById(android.R.id.content),
                     getString(R.string.application_submitted),
                     Snackbar.LENGTH_SHORT // How long to display the message.
                 ).show()
                 viewModel.doneShowingSnackbar()
+
+                binding.button.contentDescription = getString(R.string.submit)
+                binding.button.text = getString(R.string.done)
             }
         })
+
 
         setHasOptionsMenu(true)
         return binding.root
